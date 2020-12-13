0345a6746ef5d3b4b3b05eb36d35c0d944f4af96
nicoboss <nico@bosshome.ch>
2020-12-13 15:53:22 +0100

    Revert "Example how to calculate the second derivation of our measurements in python"
    This reverts commit 57fda406cdb3f8ddaad6c06f89c1bf0d357f54ab.

--- gui/server/server.py
+++ gui/server/server.py
@@ -1,5 +1,4 @@
 import socket
-import json
 while True:
 	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	server.bind(('192.168.133.79', 3333))
@@ -11,19 +10,5 @@ while True:
 	print ("[*] Recevied '", data, "' from the client")
 	print ("    Processing data")
 	client.close()
-	json_str = json.dumps(data)
-	resp = json.loads(json_str)
-	oldItem = 0
-	diff1 = []
-	for item in resp['piezo_0']:
-		diff1.append(oldItem-item)
-		oldItem = item
-	print(diff1)
-	diff2 = []
-	oldItem = 0
-	for item in diff1:
-		diff2.append(oldItem-item)
-		oldItem = item
-	print(diff2)
 	with open("test.json", "wb") as f:
 		f.write(data)
